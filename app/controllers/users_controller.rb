99b5388b0a10f4c94ea8198e10c6e218170e876c
奈良亮 <motorbicykle@gmail.com>
2020-03-13 14:25:32 +0900

    resourcesを使わないCRUD処理の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+    #一覧画面に対応するアクション
+    def index
+        @users = User.all.order(id: :asc)
+    end
+    
+    
+    #新規作成画面に対応するアクション
+    def new
+        @user = User.new
+    end
+
+    #新規登録するためのアクション
+    def create
+        User.create(user_params)
+        redirect_to :action => "index"
+    end
+
+    #編集画面に遷移するアクション
+    def edit
+        @user = User.find(params[:id])
+    end
+
+　　#データを消去するアクション
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to :action => "index"
+    end
+
+   #データを更新するアクション
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        redirect_to :action => "index"
+
+    end
+    
+    private #----------------------------コントローラー内で呼び出すメソッド------------------------
+
+    def user_params
+        #登録できる情報を制限するためのコード
+        params.require(:user).permit(:name, :age)
+    end
+
+    #送信すると、以下のパラメーターがデータベースに格納される
+    # Parameters: {"user"=>{"name"=>"nara", "age"=>"22"}}
+
+   
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#一覧画面へのルーティング
+get '/users', to: 'users#index'
+
+#新規投稿画面のルーティング
+get '/users/new', to: 'users#new'
+
+#新規登録を行うためのルーティング
+post '/users', to: 'users#create'
+
+#編集するためのルーティング
+get '/users/:id/edit', to: 'users#edit'
+
+#更新を行うためのルーティング
+patch '/users/:id', to: 'users#update'
+
+#投稿を削除するためのルーティング
+delete '/users/:id', to: 'users#destroy'
+
+
+
 end
